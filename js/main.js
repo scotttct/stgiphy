65f7cb3fbc3873605c8e84a362ab5618b6da26a9
ScottTomassetti <scott_t_ct@hotmail.com>
2020-04-02 04:37:24 -0400

    Updates

---
+++ js/main.js
@@ -0,0 +1,54 @@
+//code was modified from https://www.youtube.com/watch?v=x9Vy-wmtYic
+(function () {
+  function giphySearch(keyword) {
+    return fetch(`http://api.giphy.com/v1/gifs/search?q=${keyword}&api_key=${GIPHY_KEY}&limit=10`)
+      .then(response => response.json());
+  }
+
+  function appendImage(img) {
+    let $div = $('<div class="img-wrapper"></div>');
+    $('<div class="inner"></div>').append(img).appendTo($div);
+    $('#thumbs').append($div)
+  }
+
+  function showLoader() {
+    $('.loader-wrapper').addClass('shown');
+  }
+
+  function hideLoader() {
+    $('.loader-wrapper').removeClass('shown');
+  }
+
+  function onImgLoad(img) {
+    return new Promise((resolve, reject) => {
+      img.onload = resolve;
+    });
+  }
+
+  (function listenOnFormSubmit() {
+    $('#searchForm').submit(async (ev) => {
+      ev.preventDefault();
+
+      let $input = $('#searchInput');
+
+      main($input.val());
+    });
+  })();
+
+  async function main(keyword) {
+    const result = await giphySearch(keyword);
+    $('#thumbs').html('');
+    showLoader();
+    let promises = [];
+    //iterate through the images 
+    result.data.forEach(gif => {
+      let img = new Image();
+      img.src = gif.images.original.url;
+      promises.push(onImgLoad(img));
+      appendImage(img);
+    });
+
+    await Promise.all(promises);
+    hideLoader();
+  }
+})();
